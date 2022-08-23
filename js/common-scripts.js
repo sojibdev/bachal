53eb491eb99cc9a5d429faed6656f8d0fd62aff7
Sojib Hasan <105726245+sojibdev@users.noreply.github.com>
2022-08-23 22:30:30 +0600

    Add files via upload

---
+++ js/common-scripts.js
@@ -0,0 +1,22 @@
+
+(function($){
+	$(function(){
+
+
+
+        // Phone nav click function
+        $('.mode').click(function () {
+            $("body").toggleClass("darkMode");
+        });
+
+        
+		
+	})// End ready function.
+   
+    
+    
+
+	
+
+})(jQuery)
+
